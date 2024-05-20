f57060f1fee4d95e4f859be1b2f259729e8bf184
andrii sukhoi <andrii.sukhoi@nocowanie.pl>
2024-05-20 06:48:07 +0200

    Add treasure:write to owner.username -- update embedded fileds

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -45,7 +45,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 255, unique: true)]
-    #[Groups(['user:read', 'user:write', 'treasure:item:read'])]
+    #[Groups(['user:read', 'user:write', 'treasure:item:read', 'treasure:write'])]
     #[Assert\NotBlank]
     private ?string $username = null;
 
